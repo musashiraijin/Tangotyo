093336f73131e45c793f8c5fa1be0486507bed4e
Musashi Raijin <Raijin@raijin.local>
2016-09-02 11:10:12 +0900

    Initial Commit

---
+++ BasicEnglish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicEnglish
+//
+//  Created by Musashi Raijin on 2016/09/02.
+//  Copyright © 2016年 masahiro.kitamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
